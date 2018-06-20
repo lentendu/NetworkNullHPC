3b8494ac1d7d2dff925e46386af9f2e66545ab7e
lentendu <lentendu@rhrk.uni-kl.de>
2018-06-20 13:41:32 +0200

    Avoid empty edges blocks when reading pair names at network step

--- rscripts/network.R
+++ rscripts/network.R
@@ -24,7 +24,9 @@ stopCluster(cl)
 if(length(unlist(edges_ok_signif))>0) {
   otus<-scan("otus",what="character",quiet=T)
   net_name<-foreach(i=1:blocks,.combine=c) %do% {
-    h5read("net_name.h5","name",index=list(edges_ok_signif[[i]]$edges,i))
+	if (length(edges_ok_signif[[i]])>0) {
+	  h5read("net_name.h5","name",index=list(edges_ok_signif[[i]]$edges,i))
+	}
   }
   net_edges<-cbind(ldply(net_name,function(x) otus[as.numeric(unlist(sub("^0*","",unlist(strsplit(x,"-")))))]),ldply(edges_ok_signif)$cor)
   write.table(net_edges,file.path("..",paste("cooccurrence",config$cksum,sub("\\.[^\\.]*$","",basename(config$mat)),"txt",sep=".")),col.names=F,row.names=F,sep=" ",quote=F)
@@ -34,7 +36,9 @@ if(length(unlist(edges_ok_signif))>0) {
 
 if(length(unlist(edges_ex_ok_signif))>0) {
   net_ex_name<-foreach(i=1:blocks,.combine=c) %do% {
-    h5read(paste0(project,"net_name.h5"),"name",index=list(edges_ex_ok_signif[[i]]$edges,i))
+    if (length(edges_ex_ok_signif[[i]])>0) {
+      h5read(paste0(project,"net_name.h5"),"name",index=list(edges_ex_ok_signif[[i]]$edges,i))
+    }
   }
   net_ex_edges<-cbind(ldply(net_ex_name,function(x) otus[as.numeric(unlist(sub("^0*","",unlist(strsplit(x,"-")))))]),ldply(edges_ex_ok_signif)$cor)
   write.table(net_ex_edges,file.path("..",paste("coexclusion",config$cksum,sub("\\.[^\\.]*$","",basename(config$mat)),"txt",sep=".")),col.names=F,row.names=F,sep=" ",quote=F)
